24db0f864d7e1f21fe4597eaba46c95c76681e25
Alexandru Raul <alexandru.raul@gmail.com>
2026-03-02 14:50:17 +0200

    add deployment tags

--- variables_tag_v2.tf
+++ variables_tag_v2.tf
@@ -15,3 +15,36 @@ variable "extra_tags" {
   type        = map(string)
   default     = {}
 }
+
+########################
+# Tags - Deployment (Jenkins)
+########################
+
+variable "deployment_tags_static" {
+  description = "AWS deployment static tags"
+  type        = map(string)
+}
+
+variable "tag_build_number" {
+  description = "Build number, taken from Jenkins"
+  type        = string
+  default     = ""
+}
+
+variable "tag_build_author" {
+  description = "Deployment author, taken from Jenkins"
+  type        = string
+  default     = ""
+}
+
+variable "tag_build_hash" {
+  description = "GIT hash, taken from Jenkins"
+  type        = string
+  default     = ""
+}
+
+variable "tag_build_job_name" {
+  description = "Job name, taken from Jenkins"
+  type        = string
+  default     = ""
+}
